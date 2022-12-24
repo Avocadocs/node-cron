0423a95590bc304030b85b830d8640b51965bc5f
Avocadocs <kerian.suisini@gmail.com>
2022-12-24 10:18:30 +0100

    Connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import cron from 'node-cron';
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGO_URL;
+
+const client = new MongoClient(uri);
+
 
 cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("test").insert({test: 'nodejs'});
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 });
